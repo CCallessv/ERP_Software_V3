53b7fe7d99dc706baa651238e74a266832d1e104
David Calles <150052580+CCallessv@users.noreply.github.com>
2026-02-18 19:07:42 -0600

    Cambio en Setting especificamente en DB
    se cambio en setting la configuracion a la db ya q tenia datos locales y no se puede manejar asi

--- config/settings.py
+++ config/settings.py
@@ -75,14 +75,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-   'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'erp_db',       #  nombre de la base de datos en pgAdmin
-        'USER': 'postgres',            # Usuario de la base de datos
-        'PASSWORD': 'pass123',   
-        'HOST': 'localhost',
-        'PORT': '5432',
-    
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
